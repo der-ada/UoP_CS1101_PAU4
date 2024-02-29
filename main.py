063df0b47a90b44f1a55f09a430db205bb521212
Andreas Dau <andreas@andreasdau.com>
2024-02-29 22:27:33 +0000

    Added actual formula for the hypotenuse.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Andreas Dau, 2024-02-29
 
 def hypotenuse(kathete1: float, kathete2: float):
-    return kathete1 + kathete2
+    return (kathete1 ** 2 + kathete2 ** 2) ** 0.5
 
 
-print(hypotenuse(1, 2))
+print(hypotenuse(3, 4))
