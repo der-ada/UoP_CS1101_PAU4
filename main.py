a3ea47b4593a6f0520503cdd1180121dbee5c76a
Andreas Dau <andreas@andreasdau.com>
2024-02-29 22:18:36 +0000

    Added parameters and return their sum.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Programming Assignment Unit 4
-# Andreas Dau, 2024-02-09
+# Andreas Dau, 2024-02-29
 
-def hypotenuse():
-    return "Hello from your function ‘hypotenuse’!"
+def hypotenuse(kathete1: float, kathete2: float):
+    return kathete1 + kathete2
 
 
-print(hypotenuse())
+print(hypotenuse(1, 2))
