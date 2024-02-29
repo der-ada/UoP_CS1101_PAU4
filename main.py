24332f475260e47cbb6004c4ead345839bd151b7
Andreas Dau <andreas@andreasdau.com>
2024-02-29 22:46:35 +0000

    Changed square root to ‘sqrt’ from module ‘math.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # Programming Assignment Unit 4
 # Andreas Dau, 2024-02-29
 
+from math import sqrt
+
+
 def hypotenuse(kathete1: float, kathete2: float):
-    return (kathete1 ** 2 + kathete2 ** 2) ** 0.5
+    return sqrt(kathete1 ** 2 + kathete2 ** 2)
 
 
 print(hypotenuse(3, 4))
